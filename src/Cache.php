2a83cd7132f7eedb1aa423e3c27b33ae42a3ee19
Martin <hello@northrook.com>
2024-05-04 18:45:55 +0100

    mend

--- src/Cache.php
+++ src/Cache.php
@@ -6,6 +6,16 @@ final class Cache
 {
     private static array $objectCache = [];
 
+    public static function __callStatic( string $name, array $arguments ) {
+        return match ( $name ) {
+            'get' => ( new Cache() )->get( $arguments[ 0 ], true ),
+        };
+    }
+
+    public function get( string $key, bool $global = false ) : mixed {
+        return Cache::$objectCache[ $this->key( $key, $global ) ];
+    }
+
     public function value( string $key, mixed $value, bool $global = false ) : mixed {
 
         $key = $global ? $key : $key . ':' . spl_object_id( $this );
@@ -15,5 +25,9 @@ final class Cache
         }
 
         return Cache::$objectCache[ $key ];
+    }
+
+    private function key( string $key, bool $global = false ) : string {
+        return $global ? $key : $key . ':' . spl_object_id( $this );
     }
 }
