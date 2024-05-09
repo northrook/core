b2e4c672c32310be478697f3b7b7f1b816ad8d56
Martin <hello@northrook.com>
2024-05-09 15:16:07 +0100

    mend

---
+++ src/Exception/ClassStructureException.php
@@ -0,0 +1,32 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace Northrook\Core\Exception;
+
+use Exception;
+
+/**
+ * The exception occurred during Component compilation.
+ */
+class ClassStructureException extends Exception
+{
+
+    /**
+     * Construct the exception. Note: The message is NOT binary safe.
+     * @link https://php.net/manual/en/exception.construct.php
+     *
+     * @param string       $message  [optional] The Exception message to throw.
+     * @param string|null  $string
+     * @param int          $code     [optional] The Exception code.
+     */
+    public function __construct(
+        string                  $message = '',
+        public readonly ?string $string = null,
+        int                     $code = 422,
+    ) {
+        parent::__construct( $message, $code );
+        $this->message = $message;
+        $this->code    = $code;
+    }
+}
