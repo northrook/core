5d75eeeac64c1746b26b94b1e3d1993109aed5a1
Martin <hello@northrook.com>
2024-05-26 19:28:03 +0100

    mend

--- src/Support/Str.php
+++ src/Support/Str.php
@@ -3,6 +3,7 @@
 namespace Northrook\Core\Support;
 
 use JetBrains\PhpStorm\Pure;
+use Northrook\Core\Type\Path;
 
 final class Str
 {
@@ -14,4 +15,25 @@ final class Str
         }
         return htmlspecialchars( (string) $string, ENT_QUOTES | ENT_HTML5 | ENT_SUBSTITUTE, 'UTF-8' );
     }
+
+
+    /**
+     * @param string|Path  $path
+     * @param null|string  $scheme  'http' | 'https' | 'ftp' | 'ftps' | 'file' | null as any
+     *
+     * @return bool
+     */
+    public static function isURL( string | Path $path, ?string $scheme = 'https' ) : bool {
+
+        if ( $scheme && !str_starts_with( $path, "$scheme://" ) ) {
+            return false;
+        }
+        if ( !( str_contains( $path, "//" ) && str_contains( $path, '.' ) ) ) {
+            return false;
+        }
+
+        return (bool) filter_var( $path, FILTER_VALIDATE_URL );
+    }
+
+
 }
