a8fa3c32820dd74eaa7702c68695f011c5aae47c
Martin <hello@northrook.com>
2024-08-07 16:54:37 +0100

    mend

--- src/Exception/CompileException.php
+++ src/Exception/CompileException.php
@@ -13,14 +13,14 @@ class CompileException extends \LogicException
      * Construct the exception. Note: The message is NOT binary safe.
      * @link https://php.net/manual/en/exception.construct.php
      *
-     * @param string       $message  [optional] The Exception message to throw.
-     * @param string|null  $string
-     * @param int          $code     [optional] The Exception code.
+     * @param string  $message  [optional] The Exception message to throw.
+     * @param mixed   $context  [optional] Pass in arbitrary data relevant for debugging.
+     * @param int     $code     [optional] The Exception code.
      */
     public function __construct(
-        string                  $message = '',
-        public readonly ?string $string = null,
-        int                     $code = 422,
+        string                $message = '',
+        public readonly mixed $context = null,
+        int                   $code = 422,
     ) {
         parent::__construct( $message, $code );
         $this->message = $message;
