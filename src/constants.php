4fcb6b0c0209a4aa56426e280738f025deb46784
Martin <hello@northrook.com>
2024-09-07 08:28:32 +0100

    mend

---
+++ src/constants.php
@@ -0,0 +1,27 @@
+<?php
+
+/* Core Constants
+
+ License: MIT
+ Copyright (c) 2024
+ Martin Nielsen <mn@northrook.com>
+
+*/
+
+namespace Cache {
+
+    const
+    DISABLED  = -2,
+    EPHEMERAL = -1,
+    AUTO      = null,
+    FOREVER   = 0,
+    MINUTE    = 60,
+    HOUR      = 3600,
+    HOUR_4    = 14400,
+    HOUR_8    = 28800,
+    HOUR_12   = 43200,
+    DAY       = 86400,
+    WEEK      = 604800,
+    MONTH     = 2592000,
+    YEAR      = 31536000;
+}
