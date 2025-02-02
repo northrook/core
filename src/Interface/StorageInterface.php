5b266d824a2357bc6f91cc02bc152e3a10df8421
Martin <hello@northrook.com>
2025-02-02 14:15:31 +0000

    mend

--- src/Interface/StorageInterface.php
+++ src/Interface/StorageInterface.php
@@ -2,17 +2,36 @@
 
 namespace Core\Interface;
 
-use InvalidArgumentException;
+use Psr\Cache\InvalidArgumentException;
 
 /**
  * Interface with persistent data cache.
+ *
+ * Values stored here will never expire on their own.
+ *
+ * Interoperable with:
+ * - {@see \Psr\SimpleCache\CacheInterface} `get( $key )`, `set`, `has`, `delete`, `clear`
+ * - {@see \Symfony\Contracts\Cache\CacheInterface} `get( $key, $callback )` and `delete`
  */
 interface StorageInterface
 {
     /**
+     * Check if the Storage contains a value by `$key`.
+     *
+     * @param string $key
+     *
+     * @return bool
+     *
+     * @throws InvalidArgumentException if the `$key` is invalid
+     */
+    public function has( string $key ) : bool;
+
+    /**
      * Retrieve an existing value from storage by `$key`.
      * If no value is found,the provided callback is called.
-     * The resulting value is then stored in storage and returned.
+     * The resulting value is then set and returned.
+     *
+     *  - Changes _may_ be deferred by the implementing class.
      *
      * @template Value
      *
@@ -26,7 +45,21 @@ interface StorageInterface
     public function get( string $key, callable $callback ) : mixed;
 
     /**
-     * Unsets a {@see LocalStorage::$data} value by `$key`.
+     * Set a value by `$key`.
+     *
+     *  - Changes _may_ be deferred by the implementing class.
+     *
+     * @param string $key   the key of the item to store
+     * @param mixed  $value the value of the item to store, must be serializable
+     *
+     * @return bool true on success and false on failure
+     *
+     * @throws InvalidArgumentException if the `$key` is invalid
+     */
+    public function set( string $key, mixed $value ) : bool;
+
+    /**
+     * Unsets a value by `$key`.
      *
      * - Changes _may_ be deferred by the implementing class.
      *
@@ -37,4 +70,20 @@ interface StorageInterface
      * @throws InvalidArgumentException if the `$key` is invalid
      */
     public function delete( string $key ) : bool;
+
+    /**
+     * Unset all data values.
+     *
+     *  - Changes _may_ be deferred by the implementing class.
+     *
+     * @return bool true on success and false on failure
+     */
+    public function clear() : bool;
+
+    /**
+     * Persists any deferred changes.
+     *
+     * @return bool true on success and false on failure
+     */
+    public function commit() : bool;
 }
