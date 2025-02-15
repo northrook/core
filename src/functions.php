0999ce4a5bbe025a1d9f91fd6e49a8933c0d1e97
Martin <hello@northrook.com>
2025-02-15 11:21:29 +0000

    mend

---
+++ src/functions.php
@@ -0,0 +1,171 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Support;
+
+use Stringable;
+use LengthException;
+
+/**
+ * Normalize all slashes in a string to `/`.
+ *
+ * @param string|Stringable $path
+ *
+ * @return string
+ */
+function normalizeSlashes( string|Stringable $path ) : string
+{
+    return \str_replace( '\\', '/', (string) $path );
+}
+
+/**
+ * Normalize repeated whitespace, newlines and indentation, to a single white space.
+ *
+ * @param null|string|Stringable $string
+ *
+ * @return string
+ */
+function normalizeWhitespace( string|Stringable|null $string ) : string
+{
+    return (string) \preg_replace( '#\s+#', ' ', \trim( (string) $string ) );
+}
+
+/**
+ * # Normalise a `string` or `string[]`, assuming it is a `path`.
+ *
+ * - If an array of strings is passed, they will be joined using the directory separator.
+ * - Normalises slashes to system separator.
+ * - Removes repeated separators.
+ * - Will throw a {@see ValueError} if the resulting string exceeds {@see PHP_MAXPATHLEN}.
+ *
+ * ```
+ * normalizePath( './assets\\\/scripts///example.js' );
+ * // => '.\assets\scripts\example.js'
+ * ```
+ *
+ * @param string ...$path
+ */
+function normalizePath( string ...$path ) : string
+{
+    // Normalize separators
+    $normalized = \str_replace( ['\\', '/'], DIRECTORY_SEPARATOR, $path );
+
+    $isRelative = $normalized[0][0] === DIRECTORY_SEPARATOR;
+
+    // Implode->Explode for separator deduplication
+    $exploded = \explode( DIRECTORY_SEPARATOR, \implode( DIRECTORY_SEPARATOR, $normalized ) );
+
+    // Ensure each part does not start or end with illegal characters
+    $exploded = \array_map( static fn( $item ) => \trim( $item, " \n\r\t\v\0\\/" ), $exploded );
+
+    // Filter the exploded path, and implode using the directory separator
+    $path = \implode( DIRECTORY_SEPARATOR, \array_filter( $exploded ) );
+
+    if ( ( $length = \mb_strlen( $path ) ) > ( $limit = PHP_MAXPATHLEN - 2 ) ) {
+        $method  = __METHOD__;
+        $length  = (string) $length;
+        $limit   = (string) $limit;
+        $message = "{$method} resulted in a string of {$length}, exceeding the {$limit} character limit.";
+        $result  = 'Operation was halted to prevent overflow.';
+        throw new LengthException( $message.PHP_EOL.$result );
+    }
+
+    // Preserve intended relative paths
+    if ( $isRelative ) {
+        $path = DIRECTORY_SEPARATOR.$path;
+    }
+
+    return $path;
+}
+
+/**
+ * Checks if a given value has a `path` structure.
+ *
+ * ⚠️ Does **NOT** validate the `path` in any capacity!
+ *
+ * @param string|Stringable $string
+ * @param string            $contains [..] optional `str_contains` check
+ *
+ * @return bool
+ */
+function isPath( string|Stringable $string, string $contains = '..' ) : bool
+{
+    // Stringify scalars and Stringable objects
+    // Stringify
+    $string = \trim( (string) $string );
+
+    // Must be at least two characters long to be a path string
+    if ( ! $string || \strlen( $string ) < 2 ) {
+        return false;
+    }
+
+    // One or more slashes indicate this could be a path string
+    if ( \str_contains( $string, '/' ) || \str_contains( $string, '\\' ) ) {
+        return true;
+    }
+
+    // Any periods that aren't in the first 3 characters indicate this could be a `path/file.ext`
+    if ( \strrpos( $string, '.' ) > 2 ) {
+        return true;
+    }
+
+    // Indicates this could be a `.hidden` path
+    if ( $string[0] === '.' && \ctype_alpha( $string[1] ) ) {
+        return true;
+    }
+
+    return \str_contains( $string, $contains );
+}
+
+/**
+ * Checks if a given value has a `URL` structure.
+ *
+ * ⚠️ Does **NOT** validate the URL in any capacity!
+ *
+ * @param string|Stringable $string
+ * @param ?string           $requiredProtocol
+ *
+ * @return bool
+ */
+function isUrl( string|Stringable $string, ?string $requiredProtocol = null ) : bool
+{
+    // Stringify
+    $string = \trim( (string) $string );
+
+    // Can not be an empty string
+    if ( ! $string ) {
+        return false;
+    }
+
+    // Must not start with a number
+    if ( \is_numeric( $string[0] ) ) {
+        return false;
+    }
+
+    /**
+     * Does the string resemble a URL-like structure?
+     *
+     * Ensures the string starts with a schema-like substring, and has a real-ish domain extension.
+     *
+     * - Will gladly accept bogus strings like `not-a-schema://d0m@!n.tld/`
+     */
+    if ( ! \preg_match( '#^([\w\-+]*?[:/]{2}).+\.[a-z0-9]{2,}#m', $string ) ) {
+        return false;
+    }
+
+    // Check for required protocol if requested
+    return ! ( $requiredProtocol && ! \str_starts_with( $string, \rtrim( $requiredProtocol, ':/' ).'://' ) );
+}
+
+/**
+ * Check if the provided `$path` starts with a `/`.
+ *
+ * @param string|Stringable $path
+ *
+ * @return bool
+ */
+function isRelativePath( string|Stringable $path ) : bool
+{
+    return \str_starts_with( \str_replace( '\\', '/', (string) $path ), '/' );
+}
