26108dae6d617308f1b1e41a5a2617bc56fc50a7
Martin <hello@northrook.com>
2025-02-28 06:47:56 +0000

    mend

--- src/Interface/IconProviderInterface.php
+++ src/Interface/IconProviderInterface.php
@@ -9,15 +9,15 @@ interface IconProviderInterface
     public function has( string $icon ) : bool;
 
     /**
-     * @param string                                                         $icon
-     * @param null|string                                                    $fallback
-     * @param array<array-key, string>|bool|float|int|null[]|string|UnitEnum $attributes
+     * @param string                                                        $icon
+     * @param null|string                                                   $fallback
+     * @param null|array<array-key, ?string>|bool|float|int|string|UnitEnum ...$attributes
      *
      * @return null|View
      */
     public function get(
         string                                       $icon,
         ?string                                      $fallback = null,
-        array|bool|string|int|float|UnitEnum|null ...$attributes,
+        array|null|bool|float|int|string|UnitEnum ...$attributes,
     ) : ?View;
 }
