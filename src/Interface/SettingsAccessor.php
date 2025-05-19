908e12650668c9e3df884e8e9c785d3bc9258a07
Martin <hello@northrook.com>
2025-05-19 08:08:24 +0100

    mend

---
+++ src/Interface/SettingsAccessor.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Core\Interface;
+
+trait SettingsAccessor
+{
+    private readonly SettingsProviderInterface $settings;
+
+    /**
+     * Autowired during the instantiation process of the containing class.
+     *
+     * @internal
+     *
+     * @param SettingsProviderInterface $provider
+     *
+     * @return void
+     */
+    final public function setSettingsProvider(
+        SettingsProviderInterface $provider,
+    ) : void {
+        $this->settings = $provider;
+    }
+
+    /**
+     * @template Setting of null|array<array-key, scalar>|scalar
+     *
+     * @param string                                   $key
+     * @param null|array|bool|float|int|Setting|string $default
+     *
+     * @return null|array|bool|float|int|string
+     * @phpstan-return Setting
+     */
+    final protected function getSetting(
+        string                           $key,
+        null|array|bool|float|int|string $default,
+    ) : null|array|bool|float|int|string {
+        return $this->settings->get( $key, $default );
+    }
+}
